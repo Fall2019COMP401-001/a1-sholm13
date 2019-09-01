58229ea4c13ff44f1cdd6595cfaad0b61ebdc8e0
saraholmes <saraholmes@172.17.104.53>
2019-09-01 19:21:04 -0400

    Updates to Novice program

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -2,13 +2,33 @@ package a1;
 
 import java.util.Scanner;
 
+	
 public class A1Novice {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		//Your code follows here.
+		
+		int numCust = scan.nextInt();
+		for (int i = 0; i < numCust; i++) {
+			String f = scan.next();
+			String last = scan.next();
+			String itemName = "";
+			int numItems = scan.nextInt();
+			
+			for (i=0; i < numItems; i++) {
+				double price = 0.0;
+				itemName = scan.next();
+				double totalCost = 0.0;
+				price = numItems * scan.nextDouble();
+				totalCost += price;
+			}
+			scan.close();
+			System.out.println( f.charAt(0) + last + String.format("%.2f, totalCost"));
+				
+					
+		}	
 		
 	}
 }
