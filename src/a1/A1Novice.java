24a4686d85ef260a9b0d881b51d73de03e6b3103
saraholmes <saraholmes@172.17.104.53>
2019-09-01 20:06:59 -0400

    Novice updates

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -14,21 +14,23 @@ public class A1Novice {
 		for (int i = 0; i < numCust; i++) {
 			String f = scan.next();
 			String last = scan.next();
-			String itemName = "";
 			int numItems = scan.nextInt();
+			double totalCost = 0.0;
 			
-			for (i=0; i < numItems; i++) {
-				double price = 0.0;
-				itemName = scan.next();
-				double totalCost = 0.0;
-				price = numItems * scan.nextDouble();
+			for (int j=0; j < numItems; j++) {
+				int itemCount = scan.nextInt();
+				String itemName = scan.next();
+				double price = itemCount * scan.nextDouble();
 				totalCost += price;
-			}
-			scan.close();
-			System.out.println( f.charAt(0) + last + String.format("%.2f, totalCost"));
 				
+			}
+			
+			System.out.println( f.charAt(0) + last + String.format("%.2f", totalCost));
+			
+			
 					
 		}	
-		
+		scan.close();
 	}
+	
 }
